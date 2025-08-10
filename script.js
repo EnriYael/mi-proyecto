617e39d3920a4da440658c0d85ede2d054d8dde1
EnriYael <61963506+EnriYael@users.noreply.github.com>
2025-08-09 23:00:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonReceive = document.getElementById("button-receive");
+const buttonUndo = document.getElementById("button-undo");
+const message = document.getElementById("message");
+
+buttonReceive.addEventListener("click", () => {
+    message.textContent = "¡¡TE AMO!!"
+});
+
+buttonUndo.addEventListener("click", () => {
+    message.textContent = "¡HOLA AMOR!"
+});
