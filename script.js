be9f27fb511778ca0b5f02b17345541de72d82b7
EnriYael <61963506+EnriYael@users.noreply.github.com>
2025-08-09 23:08:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const buttonUndo = document.getElementById("button-undo");
 const message = document.getElementById("message");
 
 buttonReceive.addEventListener("click", () => {
-    message.textContent = "¡¡TE AMO!!"
+    message.textContent = "¡¡♥ TE AMO ♥!!"
 });
 
 buttonUndo.addEventListener("click", () => {
     message.textContent = "¡HOLA AMOR!"
+
 });
